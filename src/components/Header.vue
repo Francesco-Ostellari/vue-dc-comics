126c49f05415981e542c320949356aef6395c436
Francesco-Ostellari <francescoostellari@gmail.com>
2022-01-13 15:51:57 +0100

    sistemazione-style

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,7 +4,7 @@
       <img src="../assets/img/dc-logo.png" alt="">
       <ul>
         <li> <a href="#">characters</a> </li>
-        <li> <a href="#">comics</a> </li>
+        <li class="li-blue"> <a class="li-blue" href="#">comics</a> </li>
         <li> <a href="#">movies</a> </li>
         <li> <a href="#">tv</a> </li>
         <li> <a href="#">games</a></li>
@@ -40,39 +40,46 @@ export default {
     background-color: white;
     height: 100px;
     width: 100%;
+    .header-container {
+      width: 70%;
+      height: 100%;
+      margin: 0 auto;
+      align-items: center;
+      justify-content: space-between;
+      padding: 10px 0;
+        img {
+          height: 100%;
+        }
+        ul {
+          text-decoration: none;
+          li {
+            text-decoration: none;
+            display: inline-block;
+            margin-right: 20px;
+            text-transform: uppercase;
+            a {
+              text-decoration: none;
+              color: #1C1C1C;
+              font-weight: 800;
+            }
+            .li-blue {
+              color: #0282F9;
+              border-bottom: 6px solid #0282F9;
+              padding-bottom: 40px;
+            }
+          }
+        }
+    }
+
   }
 
   .flex {
     display: flex;
   }
 
-  .header-container {
-    width: 70%;
-    height: 100%;
-    margin: 0 auto;
-    align-items: center;
-    justify-content: space-between;
-    padding: 10px 0;
-  }
 
-  .header-container img {
-    height: 100%;
-  }
-  
-  .header-container ul {
-    text-decoration: none;
-  }
 
-  .header-container ul li {
-    text-decoration: none;
-    display: inline-block;
-    margin-right: 20px;
-    text-transform: uppercase;
-  }
 
-  .header-container ul li a {
-    text-decoration: none;
-    color: #1C1C1C;
-    font-weight: 800;
-  }
+
+
 </style>
